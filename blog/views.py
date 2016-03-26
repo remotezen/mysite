2d2282014c4e554ea7c1ac333da99df4129abc45
remotezen <remotezen@bell.net>
2016-03-26 13:42:29 -0400

    working on post delete

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,17 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from .forms import PostForm
 
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True)\
+        .order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -37,7 +48,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
