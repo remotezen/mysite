0863400a61c27453e6b0b57d559bd631b118a372
remotezen <remotezen@bell.net>
2016-03-26 11:35:04 -0400

    fixed fixed edit indentation trouble

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Post
+'''
+As you might've guessed PostForm is the name of
+our form
+'''
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
